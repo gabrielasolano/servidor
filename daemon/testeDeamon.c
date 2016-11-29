7d218f7421c4ee2d564e20a48b58b2c516ea2d12
Gabriela Félix Solano <gabriela.solano@aker.com.br>
2016-11-29 11:59:42 -0200

    testeDeamon.c : reestruturacao do codigo
    Codigo reestruturado para entender melhor. Ainda nao
    aceita conexao do cliente. Falta funcao responde_cliente.

--- daemon/testeDeamon.c
+++ daemon/testeDeamon.c
@@ -25,10 +25,7 @@
 
 void formato_mesagem();
 void inicia_servidor(int *sock, struct sockaddr_in *servidor, int porta);
-void aceita_socket_cliente(int *sock_cliente, int sock_servidor, struct sockaddr_in *cliente, fd_set *master_fd, int *max_fd);
-void configura_listas(fd_set *master_fd, fd_set *read_fds, int sock_servidor, int *max_fd);  
 void responde_cliente (int socket_cliente);
-/*FILE *fp;*/
     
 #define MAXQUEUE 10   /* Numero maximo de conexoes que o servidor vai esperar */
 #define BUFFERSIZE BUFSIZ
@@ -43,41 +40,59 @@ int main (int argc, char **argv)
   int porta;
   int max_fd;
   int i;
-  fd_set master_fd;
   fd_set read_fds;
+  char diretorio[2000];
+  socklen_t addrlen;
   
   /* Valida quantidade de parametros de entrada */
   if (argc != 3)
   {
-    printf("Parametros incorretos.\n");
+    printf("Quantidade incorreta de parametros.\n");
     formato_mesagem();
     return 1;
   }
   
-  /* Recupera porta do parametro de entrada */
+  /* Recupera e valida a porta */
   porta = atoi(argv[1]);
+  if (porta < 0 || porta > 65535)
+  {
+    printf("Porta invalida!");
+    formato_mesagem();
+    return 1;
+  }
   
-  /* Abre arquivo de log */
-  /*fp = fopen("logDeamon.txt", "w+");*/
+  /* Recupera e valida o diretorio */
+  /*strncpy(diretorio, argv[2], strlen(argv[2]));
+  if (strlen(argv[2]) < strlen(diretorio))
+  {
+    diretorio[strlen(argv[2])] = '\0';
+  }*/
+  /* Muda o diretorio raiz do processo para o especificado em 'diretorio' */
+  /*if (chroot(diretorio) == -1) 
+  {
+    perror("chroot()");
+    exit(1);
+  }*/
   
   /* socket > configura socket > bind > listen */
   inicia_servidor(&sock_servidor, &servidor, porta);
-  
-  /* Configuracao inicial das listas master e read */
-  configura_listas(&master_fd, &read_fds, sock_servidor, &max_fd);
-  
+
+  /* Configuracao inicial da lista de sockets read_fds */
+  FD_ZERO(&read_fds);
+  FD_SET(sock_servidor, &read_fds);
+  max_fd = sock_servidor;
+
   /* Loop principal para aceitar e lidar com conexoes do cliente */
   while (1)
   {
-    /* Adiciona as conexoes numa lista */
-    read_fds = master_fd;
-    if (select(max_fd+1, &read_fds, NULL, NULL, NULL) == -1)
+    /* Select retorna em read_fds os sockets que estao prontos para leitura */
+    if (select(max_fd + 1, &read_fds, NULL, NULL, NULL) == -1)
     {
       perror("select()");
       exit(1);
     }
     
-    /* Procura por requisicoes de leitura nas conexoes */
+    /* Percorre sockets e verifica se estao na lista read_fds para serem tratados */
     for (i = 0; i <= max_fd; i++)
     {
       /* Existe requisicao de leitura */
@@ -86,20 +101,35 @@ int main (int argc, char **argv)
         /* Socket do servidor: aceita as conexoes novas */
         if (i == sock_servidor)
         {
-          aceita_socket_cliente(&sock_cliente, sock_servidor, &cliente, &master_fd, &max_fd);
+          addrlen = sizeof(cliente);
+          sock_cliente = accept(sock_servidor, (struct sockaddr *)&cliente, &addrlen);
+          if (sock_cliente == -1)
+          {
+            perror("accept()");
+          }
+          else
+          {
+            FD_SET(sock_cliente, &read_fds);
+            if (sock_cliente > max_fd)
+              max_fd = sock_cliente;
+            printf("Nova conexao de %s no socket %d\n", inet_ntoa(cliente.sin_addr), (sock_cliente));
+          }
         }
         
         /* Socket dos clientes: lida com os dados dos cliente */
         else
         {
+          //printf("Entrou aqui\n");
           responde_cliente(sock_cliente);
-          FD_CLR(sock_cliente, &master_fd);
+          FD_CLR(sock_cliente, &read_fds);
+          //printf("Saiu aqui\n");
         }
       }
     }
   }
   return 0;
 }
+
 
 void inicia_servidor(int *sock, struct sockaddr_in *servidor, int porta)
 {
@@ -140,37 +170,6 @@ void inicia_servidor(int *sock, struct sockaddr_in *servidor, int porta)
   }
 }
 
-void configura_listas (fd_set *master_fd, fd_set *read_fds, int sock_servidor, int *max_fd)
-{
-  /* Zera as lista master e read (file descriptors) */
-  FD_ZERO(&(*master_fd));
-  FD_ZERO(&(*read_fds));
-  
-  /* Adiciona socket_servidor na lista master */
-  FD_SET(sock_servidor, &(*master_fd));
-  
-  /* Controle dos sockets (file descriptors) */
-  (*max_fd) = sock_servidor;  
-}
-
-void aceita_socket_cliente(int *sock_cliente, int sock_servidor, struct sockaddr_in *cliente, fd_set *master_fd, int *max_fd)
-{
-  socklen_t addrlen = sizeof(*cliente);
-  (*sock_cliente) = accept(sock_servidor, (struct sockaddr *)&(*cliente), &addrlen);
-  if ((*sock_cliente) == -1)
-  {
-    perror("accept()");
-  }
-  else
-  {
-    /* Adiciona o novo socket na lista master, atualiza o fd máximo */
-    FD_SET((*sock_cliente), &(*master_fd));
-    if ((*sock_cliente) > (*max_fd))
-      (*max_fd) = (*sock_cliente);
-    printf("Nova conexao de %s no socket %d\n", inet_ntoa((*cliente).sin_addr), (*sock_cliente));
-  }
-}
-
  /*!
  * Funcao para tratar o recebimento de mensagens no socket de um determinado
  * cliente n e enviar a resposta adequada.
@@ -180,10 +179,15 @@ void responde_cliente (int sock_cliente)
   int bytes;
   char buffer[BUFFERSIZE];
   
+  /* 1. Servidor recebe a mensagem do socket cliente
+   * 2. Servidor responde a mensagem para o socke cliente
+   * 3. Servidor fecha o socket cliente
+   */
+  
   while ((bytes = recv((sock_cliente), buffer, sizeof(buffer), 0)) > 0)
   {
     printf("Entrou aqui!\nBuffer:\n%s", buffer);
-    /*send(sock_cliente, "Ola", 4, 0);*/
+    send(sock_cliente, "Mundo", 4, 0);
   }
           
   if (bytes <= 0)
@@ -207,15 +211,12 @@ void responde_cliente (int sock_cliente)
   char *page;
   int tmp;
   int headerSize = 0;
-
   memset(buf, 0, sizeof(buf));
-
   if (clientsContext[n].active == 0)
   {
     clientsContext[n].active = 1;
     activeClients++;
   }
-
   if (clientsContext[n].msgReceived == 0)
   {
     if ((tmp = recv(clients[n], buf, sizeof(buf), 0)) > 0)
@@ -223,7 +224,6 @@ void responde_cliente (int sock_cliente)
       memcpy(clientsContext[n].msg + clientsContext[n].received, buf, tmp);
       clientsContext[n].received += tmp;
       memset(buf, 0, sizeof(buf));
-
       if(strstr(clientsContext[n].msg, "\r\n\r\n") != NULL)
       {
         clientsContext[n].msgReceived = 1;
@@ -233,7 +233,6 @@ void responde_cliente (int sock_cliente)
         return;
       }
     }
-
     if (tmp == 0)
     {
       fprintf(logfile, "Socket closed\n");
@@ -247,13 +246,11 @@ void responde_cliente (int sock_cliente)
       return;
     }
   }
-
   if (clientsContext[n].writting == 0)
   {
     fprintf(logfile, "Message received from socket %d:\n\n%s\n", clients[n],
           clientsContext[n].msg);
     aux = strtok_r(clientsContext[n].msg, " ", &context);
-
     if (strncmp(aux, "GET", 3) != 0)
     {
       aux = strtok_r(NULL, " ", &context);
@@ -278,7 +275,6 @@ void responde_cliente (int sock_cliente)
     {
       page = strtok_r(NULL, " ", &context);
       aux = strtok_r(NULL, "\r", &context);
-
       if ((strncmp(aux, "HTTP/1.0", 8) != 0) &&
           (strncmp(aux, "HTTP/1.1", 8) != 0))
       {
@@ -287,12 +283,10 @@ void responde_cliente (int sock_cliente)
         closeClient(n);
         return;
       }
-
       if (file_exist(page))
       {
         stat(page, &st);
         clientsContext[n].toSend = st.st_size;
-
         if ((clientsContext[n].fp = fopen(page,"rb")) == NULL)
         {
           fprintf(logfile, "fopen() error\n");
@@ -301,21 +295,15 @@ void responde_cliente (int sock_cliente)
           closeClient(n);
           return;
         }
-
         memset(buf, '\0', sizeof(buf));
-
         time_t now = time(0);
         struct tm tm = *gmtime(&now);
         strftime(date, sizeof(date), "%a, %d %b %Y %H:%M:%S %Z", &tm);
         sprintf(buf, "HTTP/1.0 200 OK\nDate: %s\nConnection: "
                      "Closed\r\n\r\n", date);
-
         headerSize = strlen(buf);
-
-
         clientsContext[n].read = fread(buf + headerSize, 1, (sizeof(buf) -
                                        headerSize), clientsContext[n].fp);
-
         tmp = send(clients[n], buf, (clientsContext[n].read + headerSize), MSG_NOSIGNAL);
         if (tmp <= 0)
         {
@@ -323,15 +311,11 @@ void responde_cliente (int sock_cliente)
           closeClient(n);
           return;
         }
-
         clientsContext[n].writting = 1;
-
         clientsContext[n].sent += tmp;
         memset(buf, 0, sizeof(buf));
-
         fprintf(logfile, "%d bytes sent on socket %d\n", clientsContext[n].sent,
                 clients[n]);
-
         if (clientsContext[n].sent >= clientsContext[n].toSend)
         {
           closeClient(n);
@@ -351,22 +335,17 @@ void responde_cliente (int sock_cliente)
   else
   {
     clientsContext[n].read = fread(buf, 1, sizeof(buf), clientsContext[n].fp);
-
     tmp = send(clients[n], buf, (clientsContext[n].read + headerSize), MSG_NOSIGNAL);
-
     if (tmp <= 0)
     {
       fprintf(logfile, "write() error\n");
       closeClient(n);
       return;
     }
-
     clientsContext[n].sent += tmp;
     memset(buf, 0, sizeof(buf));
-
     fprintf(logfile, "%d bytes sent on socket %d\n", clientsContext[n].sent,
                 clients[n]);
-
     if (clientsContext[n].sent >= clientsContext[n].toSend)
     {
       closeClient(n);
@@ -379,6 +358,5 @@ void responde_cliente (int sock_cliente)
 void formato_mesagem ()
 {
   printf("Formato: ./recuperador <porta> <diretorio>\n");
+  printf("Portas validas: 0 a 65535\n");
 }
-
-
